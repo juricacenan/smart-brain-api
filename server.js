697afc13f30bef933a1fd7efffe29916730a876f
juricacenan <juricacenan@outlook.com>
2020-04-08 22:37:11 +0200

    heroku pg host change

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controlers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-triangular-58959',
       user : 'postgres',
       password : 'jurecenan1',
       database : 'smart-brain'
